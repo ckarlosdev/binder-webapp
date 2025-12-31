d6a9d00ace4d1b91e66dc263b729242f53d1a8a2
Carlos Perez <per.ckarlos@gmail.com>
2025-12-31 11:27:53 -0600

    router configuratio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,19 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import Home from "./Home";
 import Binder from "../components/Binder";
 
-const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/binder/:jobsId", element: <Binder /> },
+const router = createHashRouter(
+  [
+    { path: "/", element: <Home /> },
+    { path: "/binder/:jobsId", element: <Binder /> },
+    {
+      path: "*",
+      element: <div>404 | Page not found</div>,
+    },
+  ],
   {
-    path: "*",
-    element: <div>404 | Página no encontrada</div>,
-  },
-]);
+    basename: import.meta.env.BASE_URL,
+  }
+);
 
 export default router;
